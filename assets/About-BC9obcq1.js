9e79be87f24f6986e6b22611c53e7f0fe7fa606c
DanyloShyshkovskyi <DanyloShyshkovskyi@users.noreply.github.com>
2024-05-03 10:49:38 +0000

    deploy: 3c6c284ae53e6988522f70717cbaf8b81799b64b

---
+++ assets/About-BC9obcq1.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CT3-tdsj.js";const o=()=>t.jsx("div",{children:t.jsx("h1",{className:"my-20 text-center text-5xl font-bold",children:"About"})});export{o as default};
---
+++ assets/Home-BTz7waBh.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CT3-tdsj.js";const o=()=>e.jsx("div",{children:e.jsx("h1",{className:"my-20 text-center text-5xl font-bold",children:"Homepage"})});export{o as default};
