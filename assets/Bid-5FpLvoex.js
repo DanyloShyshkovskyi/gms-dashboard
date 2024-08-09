2dc56b4964cc332e846cf6a7e880c5072255f236
DanyloShyshkovskyi <DanyloShyshkovskyi@users.noreply.github.com>
2024-08-09 18:16:46 +0000

    deploy: d56683d62aed9a86cc5c8d568614a138da476385

---
+++ assets/Bid-5FpLvoex.js
@@ -0,0 +1,1 @@
+import{u as t,j as o}from"./main-l2mZ9PI0.js";const s=()=>{const n=t();return console.log(n),o.jsx(o.Fragment,{children:o.jsx("h1",{children:"Component"})})};export{s as Component};
--- assets/Bids-CriYhwUd.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-Cb-l_8yO.js";const s=()=>t.jsx("div",{children:t.jsx("h1",{className:"my-20 text-center text-5xl font-bold text-indigo-800",children:"Bids"})});export{s as default};
