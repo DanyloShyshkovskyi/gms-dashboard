fc835921e669647d73d6a6c4a021c4caa87c83f4
DanyloShyshkovskyi <DanyloShyshkovskyi@users.noreply.github.com>
2024-08-16 17:33:34 +0000

    deploy: f789d64e734431fd21144bc293da2c77e943e125

--- assets/Bid.js
+++ assets/Bid.js
@@ -1,1 +1,1 @@
-import{m as t,j as o}from"./main2.js";const s=()=>{const n=t();return console.log(n),o.jsx(o.Fragment,{children:o.jsx("h1",{children:"Component"})})};export{s as Component};
+import{o as t,j as o}from"./main2.js";const s=()=>{const n=t();return console.log(n),o.jsx(o.Fragment,{children:o.jsx("h1",{children:"Component"})})};export{s as Component};
